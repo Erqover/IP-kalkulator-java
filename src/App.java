f7afba0b5e5e0f9961da6a73c983fd534d24ea19
Erqover <erqover.armcrode@gmail.com>
2023-03-10 21:54:11 +0100

    1.3v (zmaina tablic na listy, przebudowa konstruktora)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        List<Integer> j1 = new  ArrayList<>(){{
+            add(192);
+            add(168);
+            add(1);
+            add(1);
+        }};
+        List<Integer> j2 = new  ArrayList<>(){{
+            add(255);
+            add(255);
+            add(255);
+            add(0);
+        }};
         
+        Kalk prog = new Kalk(j1, j2);
+        prog.outtest();
     }
 }
--- src/Kalk.java
+++ src/Kalk.java
@@ -1,18 +1,31 @@
-public class Kalk {
-    private String[] ip_bin;
-    private String[] mask_bin;
-    private int[] ip_dec;
-    private int[] mask_dec;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Kalk(int[] ip_dec, int[] mask_dec) {
+public class Kalk {
+    private List<String> ip_bin = new ArrayList<>();
+    private List<String> mask_bin = new ArrayList<>();
+    private List<Integer> ip_dec = new ArrayList<>();
+    private List<Integer> mask_dec = new ArrayList<>();
+
+    public Kalk(List<Integer> ip_dec, List<Integer> mask_dec) {
         this.ip_dec = ip_dec;
         this.mask_dec = mask_dec;
-        for (int i=0;i < 4;i++) {
-            while (ip_dec[i] >= 0) {
-                if (ip_dec[i]%2 == 1) {
-                    this.ip_bin
+        String temp = "";
+        for (int i=0;i<4;i++) {
+            while (ip_dec.get(i) >= 0) {
+                if (ip_dec.get(i) % 2 == 1) {
+                    temp.concat("1");
+                }
+                else {
+                    temp.concat("0");
                 }
             }
+            ip_bin.set(i, temp);
         }
+    }
+
+    public void outtest() {
+        System.out.println(ip_dec.toString());
+        System.out.println(mask_dec.toString());
     }
 }
